169d3c660911adc208a6b8d93bdacf0c4ba6e1b5
Arunr1022 <arunhhh@gmail.com>
2025-04-23 23:12:32 +0530

    Working on Router and link concepts

---
+++ src/components/Carts/Lastofus.jsx
@@ -0,0 +1,13 @@
+import Lou from '../images/wp9723146-last-of-us-4k-phone-wallpapers.jpg';
+
+function Lastofus() {
+
+    return(
+        <>
+        <img src={Lou} alt="..." />
+        </>
+    );
+    
+}
+
+export default Lastofus
