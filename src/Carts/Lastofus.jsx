d07351886121134792784225364e5835941712fd
Arunr1022 <arunhhh@gmail.com>
2025-04-24 17:30:26 +0530

    making components

--- src/Carts/Lastofus.jsx
+++ src/Carts/Lastofus.jsx
@@ -2,6 +2,17 @@ import Lou from '../images/separateimages/wp2243855-tlou-wallpapers.jpg';
 
 function Lastofus() {
 
+    const systemreq = [
+        {
+            id : 1,
+            cpu : "Intel Core i7 4770K AMD Ryzen 5 1500X",
+            ram : "16GB",
+            gpu : "Nvidia GeForce GTX 970 AMD Radeon RX 470/ Nvidia GeForce GTX 1050/, Ti AMD Radeon RX 5600 XT",
+            storage : "100GB SSD"
+        }
+    ];
+
+
     return(
         <>
         <div className="Lastofuscontainer">
@@ -13,10 +24,15 @@ function Lastofus() {
         <h2 className='GameHeading'>Last Of Us</h2>
         <p className='p1'>Full Edition</p>
         <p className="p2">System Requirements</p>
-        <p className="p3">CPU       : Intel Core i7 4770K AMD Ryzen 5 1500X</p>
-        <p className="p4">RAM       : 16GB</p>
-        <p className="p5">GPU       : Nvidia GeForce GTX 970 AMD Radeon RX 470, Nvidia GeForce GTX 1050, Ti AMD Radeon RX 5600 XT</p>
-        <p className="p6">Storage   : 100GB SSD</p>
+        {systemreq.map((item, id)=>(
+        <div key={id}>
+        <p className='p3'>Cpu : {item.cpu}</p>
+        <p className='p4'>RAM : {item.ram}</p>
+        <p className='p5'>Gpu : {item.gpu}</p>
+        <p className='p6'>Storage : {item.storage}</p>       
+        </div>
+        ))}
+        <button className='btn btn-primary b1'>Buy Now</button>
         </div>
         </div>
         </>
