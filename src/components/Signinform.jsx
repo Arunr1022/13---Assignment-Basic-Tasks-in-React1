63b7e4a6a4a8d79efa36eee81f109872f8a0fe67
Arunr1022 <arunhhh@gmail.com>
2025-04-26 00:55:53 +0530

    succesfully added signin and signup form with validation

--- src/components/Signinform.jsx
+++ src/components/Signinform.jsx
@@ -1,21 +1,51 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Signin() {
 
+    const [email, setEmail] = useState('');
+    const [userpassword, setUserpassword] = useState('');
+
+
+    const handleSubmit = (e) => {
+
+        if(!email){
+            alert("Email Id is required !");
+            return;
+        }
+
+        if(!/\S+@\S+\.\S+/.test(email)){
+            alert("Please enter the valid email ID!");
+            return;
+        }
+
+        if(!userpassword){
+            alert("Password field id empty");
+            return;
+        }
+
+        alert("Logged in Successfully");
+
+    };
+
+
     return (
         <>
-            <form className="signinpage">
+            <form className="signinpage" onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="InputEmail1" className="form-label">Email address : </label>
-                    <input type="email" className="form-control" id="InputEmail1"></input>
+                    <input type="email" className="form-control" id="InputEmail1" value={email} onChange={(e) => setEmail(e.target.value) } ></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="InputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="InputPassword1"></input>
+                    <input type="password" className="form-control" id="InputPassword1" value={userpassword} onChange={ (e) => setUserpassword(e.target.value) }></input>
                 </div>
                 <button type="submit" className="btn btn-primary">Sign In</button>
                 <Link to="/Signup">
                 <div className="Newaccountpage">Need an account ?</div>
+                </Link>
+                <Link to="/">
+                <div className="Homepage">Home</div>
                 </Link>
             </form>
         </>
--- src/components/Signupform.jsx
+++ src/components/Signupform.jsx
@@ -1,29 +1,75 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 function Signup() {
+
+
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [mobilenum, setMobilenum] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmpassword, setConfirmpassword] = useState('');
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+
+        if(username.length <=5 || username == "" ){
+            alert("Invalid Username, Username should be aleast 5 Characters.")
+            return;
+        }
+
+        if (!/[@]/.test(email) || !/\.(com|org|net)$/.test(email)) {
+            alert("Invalid Email ID");
+            return;
+        } 
+
+        if(mobilenum.length !== 10){
+            alert("Invalid Mobile Number, Enter the 10 Digit Mobile Number!");
+            return;
+        }
+
+        if(password.length <=10 || !/[A-Z]/.test(password) || !/\d/.test(password) || !/[!@#$%^&*]/.test(password)){
+            alert("Invalid Password");
+            return;
+        }
+
+        if(confirmpassword !== password){
+            alert("Password is not same");
+            return;
+        }
+
+        alert("Account Created Successfully");
+    
+    }
 
     return(
         <>
-            <form className="signuppage">
+            <form className="signuppage" onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="Username1" className="form-label">Username : </label>
-                    <input type="email" className="form-control" id="Username1"></input>
+                    <input type="text" className="form-control" id="Username1" value={username} onChange={ (e) => setUsername(e.target.value) }></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="InputEmail1" className="form-label">Email ID : </label>
-                    <input type="password" className="form-control" id="InputEmail1"></input>
+                    <input type="text" className="form-control" id="InputEmail1" value={email} onChange={ (e) => setEmail(e.target.value) }></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="Mnumber1" className="form-label">Mobile Number : </label>
-                    <input type="number" className="form-control" id="Mnumber1"></input>
+                    <input type="text" className="form-control" id="Mnumber1" value={mobilenum} onChange={ (e) => setMobilenum(e.target.value) }></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="InputPassword1" className="form-label">Password : </label>
-                    <input type="password" className="form-control" id="InputPassword1"></input>
+                    <input type="password" className="form-control" id="InputPassword1" value={password} onChange={ (e) => setPassword(e.target.value) }></input>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="InputPassword1" className="form-label">Confirm Password : </label>
-                    <input type="password" className="form-control" id="InputPassword1"></input>
+                    <input type="password" className="form-control" id="InputPassword2" value={confirmpassword} onChange={ (e) => setConfirmpassword(e.target.value) }></input>
                 </div>                
                 <button type="submit" className="btn btn-primary">SignUp</button>
+                <Link to="/">
+                <div className="Homepage">Home</div>
+                </Link>
             </form>
         </>
     );
